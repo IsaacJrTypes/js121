e996cafe94b95d1afd7a1a46ea82a4c783bedc06
Isaac J <duckhunterjr@gmail.com>
2021-04-23 17:22:07 -0700

    adds working assignment 2

--- week3/hw2/js/assignment2.js
+++ week3/hw2/js/assignment2.js
@@ -1,7 +1,16 @@
-//Prompt a message and store input in a variable
-var userInputName = prompt('Let\'s play the sum game.\n Please enter your name');
-
+//prompt a message and store user input in a variable
+var userInputName = prompt('Let\'s play the sum game.\nPlease enter your name');
 var userInputNum1 = prompt('Please enter first number');
-
 var userInputNum2 = prompt('Now enter second number');
 
+//create var to store sum of two parseInt variables 
+var totalSum = parseInt(userInputNum1) + parseInt(userInputNum2);
+
+//create var which stores concatinated message with user input variables
+var message = 'Hi, ' + userInputName + '! ' + 'The sum of ' + userInputNum1 + ' + ' + userInputNum2 + ' is ' + totalSum;
+
+//create var to hold method .getElementByid for message
+var getElement = document.getElementById('response');
+
+//replaces text content with concatinated message on line 10
+getElement.textContent = message;
