77c9de5238152a2bdd8eaf0ff24865ed8252282f
Isaac J <duckhunterjr@gmail.com>
2021-05-05 16:04:07 -0700

    add optimized hw5

--- week5/hw5/js/assignment5.js
+++ week5/hw5/js/assignment5.js
@@ -1,20 +1,18 @@
 //prompt a message and store user input in a variable
 //need too use an alert to display average score
 
-
 //need var to store: average, number of input scores, score total
-var inputTotal = -999;
-var inputCount = -1;
-var average;
-var entry;
+var inputTotal = 0,
+  inputCount = 0,
+  average,
+  entry;
 
-do{
-    entry = parseInt(prompt('Enter test score \nOr enter 999 to end entries'));
-    inputTotal += entry;
-    inputCount++;
-    var average = inputTotal / inputCount;
-    console.log(inputTotal + "," + inputCount)
-}while(entry != 999);
+do {
+  entry = parseInt(prompt("Enter test score \nOr enter 999 to end entries"));
+  inputTotal += entry;
+  inputCount++;
+  var average = (inputTotal - 999) / (inputCount - 1);
+  console.log(inputTotal + "," + inputCount);
+} while (entry != 999);
 
-
-alert("Average number is " + average);
+alert("Average score is " + average);
