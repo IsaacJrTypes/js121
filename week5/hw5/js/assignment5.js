f180cfb305abfc03e3c4c41004caba1920cc7121
Isaac J <duckhunterjr@gmail.com>
2021-05-04 18:13:06 -0700

    add hw assignment 5 (in progress)

---
+++ week5/hw5/js/assignment5.js
@@ -0,0 +1,14 @@
+//prompt a message and store user input in a variable
+var userInputNum = prompt('Enter test score \nOr enter 999 to end entries');
+
+var parsedInput = parseInt(userInputNum)
+
+function sumloop() {
+                                       
+}
+
+if(parseInt === 999){
+    return
+} else {
+
+}
